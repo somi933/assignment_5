f8fe3325257cf5f42d731284d205fd40806489b8
somi933 <forcoding108@gmail.com>
2025-09-17 22:38:01 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,5 @@
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+extract_element = numbers_list[:5]
+reverse_element = extract_element[::-1]
+print("Extracted first five elements: ",extract_element)
+print("Reversed extracted elements: ", reverse_element)
